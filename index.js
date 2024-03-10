8f1d3800727d138fd00f2084cdec14c848ec4f89
yuvansankar-dev <148607533+yuvansankar-dev@users.noreply.github.com>
2024-03-10 17:58:53 +0530

    Create index file

---
+++ index.js
@@ -0,0 +1,23 @@
+import { format } from "date-fns";
+import express from "express";
+import fs from "fs";
+const app = express();
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`App listening to the port ${PORT}`);
+});
+
+let path;
+
+app.get("/write-file", (req, res) => {
+  const data = format(new Date(), "dd-mm-yyyy hh-mm-ss");
+  path = "TimeStamp/" + data + ".txt";
+  fs.writeFileSync(path, data, "utf8");
+  res.send("File Created Successfully");
+});
+
+app.get("/read-file", (req, res) => {
+  const data = fs.readFileSync(path);
+  res.send(data);
+});
